0722650294f2fa60a3000dd825a32c217e88372f
JueCheng <JueCheng@Fly.com>
2022-03-31 15:44:55 +0800

    Second code commit

--- src/Code/FirstCode.java
+++ src/Code/FirstCode.java
@@ -4,5 +4,6 @@ public class FirstCode {
     public static void main(String[] args){
         System.out.println("本地库的创建");
         System.out.println("1.初始化本地库"+"/n"+" 2.添加暂存区"+"/n"+"3.提交本地库");
+        System.out.println("本地库的实现完成");
     }
 }
