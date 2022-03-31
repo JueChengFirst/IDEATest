6a96d8d572bfae01c46eee93d90d626449f4e468
JueCheng <JueCheng@Fly.com>
2022-03-31 16:38:36 +0800

    Sixth code commit

--- src/Code/FirstCode.java
+++ src/Code/FirstCode.java
@@ -15,5 +15,8 @@ public class FirstCode {
         System.out.println("分支改变数据");
         System.out.println("在远程库进行了数据操作");
         System.out.println("在本地库进行了数据操作");
+
+
+        System.out.println("=====");
     }
 }
