55b4b37ba7376c2bfe28b4d0124dc8a6f1ae7f9c
JueCheng <JueCheng@Fly.com>
2022-03-31 16:12:06 +0800

    Forth code commit

--- src/Code/FirstCode.java
+++ src/Code/FirstCode.java
@@ -5,9 +5,11 @@ public class FirstCode {
         System.out.println("本地库的创建");
         System.out.println("1.初始化本地库"+"/n"+" 2.添加暂存区"+"/n"+"3.提交本地库");
         System.out.println("本地库的实现完成");
+        System.out.println("master 改变数据");
         System.out.println("分支操作");
         System.out.println("分支1 开始工作一次");
         System.out.println("分支1 开始工作两次");
         System.out.println("分支1 开始工作三次");
+        System.out.println("分支1 改变数据");
     }
 }
