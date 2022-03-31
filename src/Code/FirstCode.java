1e2c4ca4252ea8799f25e0d16b5fb20bb8ac2604
JueCheng <JueCheng@Fly.com>
2022-03-31 15:55:12 +0800

    branch1 Second code commit

--- src/Code/FirstCode.java
+++ src/Code/FirstCode.java
@@ -6,6 +6,8 @@ public class FirstCode {
         System.out.println("1.初始化本地库"+"/n"+" 2.添加暂存区"+"/n"+"3.提交本地库");
         System.out.println("本地库的实现完成");
         System.out.println("分支操作");
-        System.out.println("分支1 开始工作");
+        System.out.println("分支1 开始工作一次");
+        System.out.println("分支1 开始工作两次");
+        System.out.println("分支1 开始工作三次");
     }
 }
